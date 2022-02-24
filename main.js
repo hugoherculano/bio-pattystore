bef13f3d59de74abbcef82c257e5e7b83357ba26
hugoherculano <hugohercullano@gmail.com>
2022-02-24 17:40:05 -0300

    inicio do catalogo

---
+++ main.js
@@ -0,0 +1,9 @@
+
+const fundo = document.querySelector('.modal-full');
+const fundoFalso = document.querySelector('.fundo');
+const listaPrices = document.querySelector('')
+
+
+fundoFalso.addEventListener('click', () => {
+    fundo.style.display = 'none'
+})
