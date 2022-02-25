09c105d08ac9c2ab5c6e40a8cd92ddeafa32b18e
hugoherculano <hugohercullano@gmail.com>
2022-02-25 18:50:28 -0300

    catalago finalizado!

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
 
+const arrProducts = [
+    {img: "imgs/01.png", price: "R$ 50,00"},
+    {img: "imgs/02.png", price: "R$ 50,00"},
+    {img: "imgs/03.png", price: "R$ 50,00"},
+    {img: "imgs/04.png", price: "R$ 50,00"}
+]
+
 const fundo = document.querySelector('.modal-full');
-const fundoFalso = document.querySelector('.fundo');
-const listaPrices = document.querySelector('')
+const fundoFalso = document.querySelector('.close');
+const listaPrices = document.querySelectorAll('.li-modal');
+const contModal = document.querySelector('.modalCont');
+
+const imgModal = document.querySelector('.modalCont img');
+const priceModal = document.querySelector('.modalCont section p');
 
 
 fundoFalso.addEventListener('click', () => {
-    fundo.style.display = 'none'
+    fundo.style.display = 'none';
 })
+
+for(const li of listaPrices) {
+    li.addEventListener('click', function() {
+        const idProduct = li.getAttribute('data');
+
+        imgModal.removeAttribute('src')
+        imgModal.setAttribute('src', arrProducts[idProduct].img);
+
+        priceModal.innerHTML = arrProducts[idProduct].price;
+
+        fundo.style.display = 'block'
+        
+    })
+}
